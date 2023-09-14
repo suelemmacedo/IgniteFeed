1fc634eddeed50c801d8c2a9955320f675f16719
Suelem Macedo <suelemmacedo_@hotmail.com>
2023-09-14 00:02:27 -0300

    Finalizando o Sidebar

--- IgniteFeed/src/App.jsx
+++ IgniteFeed/src/App.jsx
@@ -1,13 +1,18 @@
 import { Post } from "./Post";
 import { Header } from "./components/Header";
 import './global.css';
+import styles from './App.module.css';
+import { Sidebar } from "./components/Sidebar";
 /* Todos os componentes de uma aplicação React precisam ter extensão JSX, que nada mais é que JS + XML(HTML).*/
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post 
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+        <Post 
         author="Diego Fernandes" 
         content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit, cum! Molestiae inventore perspiciatis porro corrupti dolores corporis velit, officiis, reprehenderit sint ex aut libero ducimus quo aliquid hic numquam placeat."
       />
@@ -15,7 +20,13 @@ export function App() {
         author="Suelem Macedo" 
         content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit, cum! Molestiae inventore perspiciatis porro corrupti dolores corporis velit, officiis, reprehenderit sint ex aut libero ducimus quo aliquid hic numquam placeat."
       />
+        </main>
+      </div>
+      
+      
     </div>
   )
 }
+
+
 
---
+++ IgniteFeed/src/components/Sidebar.jsx
@@ -0,0 +1,22 @@
+import { PencilLine } from 'phosphor-react';
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img 
+        className={styles.cover} 
+        src="https://images.unsplash.com/photo-1506452819137-0422416856b8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"/>
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/108957947?v=4"/>
+        <strong>Suelem Macedo</strong>
+        <span>Web Developer</span>
+      </div>
+      <footer>
+        <a href="#">
+          <PencilLine size={20}/>
+          Editar seu perfil</a>
+      </footer>
+    </aside>
+  );
+}
