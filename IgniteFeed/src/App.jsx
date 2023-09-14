d2d44762a9a920b9452a7b9a8c009af0d93ce2c8
Suelem Macedo <suelemmacedo_@hotmail.com>
2023-09-14 06:07:25 -0300

    Starting Post Component

--- IgniteFeed/src/App.jsx
+++ IgniteFeed/src/App.jsx
@@ -1,8 +1,10 @@
-import { Post } from "./Post";
+import { Post } from "./components/Post";
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+
 import './global.css';
 import styles from './App.module.css';
-import { Sidebar } from "./components/Sidebar";
+
 /* Todos os componentes de uma aplicação React precisam ter extensão JSX, que nada mais é que JS + XML(HTML).*/
 
 export function App() {
@@ -12,14 +14,8 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-        <Post 
-        author="Diego Fernandes" 
-        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit, cum! Molestiae inventore perspiciatis porro corrupti dolores corporis velit, officiis, reprehenderit sint ex aut libero ducimus quo aliquid hic numquam placeat."
-      />
-      <Post 
-        author="Suelem Macedo" 
-        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit, cum! Molestiae inventore perspiciatis porro corrupti dolores corporis velit, officiis, reprehenderit sint ex aut libero ducimus quo aliquid hic numquam placeat."
-      />
+        <Post />
+        <Post />
         </main>
       </div>
       
---
+++ IgniteFeed/src/components/Post.jsx
@@ -0,0 +1,30 @@
+import styles from "./Post.module.css";
+
+
+export function Post() {
+   return  (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/108957947?v=4"/>
+          <div className={styles.authorInfo}>
+            <strong>Suelem Macedo</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time title="14 de setembro às 05:51h" dateTime="2023-09-14 05:51">Publicado há 1h</time>
+      </header>
+      <div className={styles.content}>
+      <p>Fala galeraa 👋</p>
+
+      <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+      <p>👉 <a href="">jane.design/doctorcare</a></p>
+
+      <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+        
+      </div>
+
+    </article>
+   )
+}
