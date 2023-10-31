f59f2cb5180cead2103774d9619c61a561e9f286
Suelem Macedo <suelemmacedo_@hotmail.com>
2023-10-30 23:15:16 -0300

    Concluindo Estrutura da Aplicação

--- IgniteFeed/src/components/Sidebar.jsx
+++ IgniteFeed/src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { PencilLine } from 'phosphor-react';
 import styles from "./Sidebar.module.css";
+import { Avatar } from './Avatar';
 
 export function Sidebar() {
   return (
@@ -8,7 +9,7 @@ export function Sidebar() {
         className={styles.cover} 
         src="https://images.unsplash.com/photo-1506452819137-0422416856b8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"/>
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/108957947?v=4"/>
+        <Avatar src="https://github.com/suelemmacedo.png" />
         <strong>Suelem Macedo</strong>
         <span>Web Developer</span>
       </div>
