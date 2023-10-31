d8d512b88b55114dfacccd02f68a4451c377d785
Suelem Macedo <suelemmacedo_@hotmail.com>
2023-10-31 18:34:13 -0300

    Iterando no JSX

--- IgniteFeed/src/App.jsx
+++ IgniteFeed/src/App.jsx
@@ -7,6 +7,42 @@ import styles from './App.module.css';
 
 /* Todos os componentes de uma aplicação React precisam ter extensão JSX, que nada mais é que JS + XML(HTML).*/
 
+
+//author: {avatar_url: "", name: "", role:""}
+//publishedAt: Date
+//content: ""
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/suelemmacedo.png",
+      name: "Suelem Macedo",
+      role: "Web Developer"
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋',},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',},
+      {type: 'link', content: 'jane.design/doctorcare',},
+    ],
+    publishedAt: new Date('2023-10-31 18:04:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO @ Rocketseat"
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋',},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',},
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-10-15 12:04:00'),
+  }
+]
+
 export function App() {
   return (
     <div>
@@ -14,8 +50,16 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-        <Post />
-        <Post />
+          {posts.map(post => {
+            return (
+              // eslint-disable-next-line react/jsx-key
+              <Post 
+                author = { post.author}
+                content = { post.content }
+                publishedAt = { post.publishedAt }
+              />
+            )
+          })}
         </main>
       </div>
       
